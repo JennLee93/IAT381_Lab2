78357a3faec372007ea8ee2182b7b9d23af2833e
JennLee93 <jennifer93lee@gmail.com>
2015-01-14 15:48:37 -0800

    initial commit

---
+++ app/scripts/controllers/faqs.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myappApp.controller:FaqsCtrl
+ * @description
+ * # FaqsCtrl
+ * Controller of the myappApp
+ */
+angular.module('myappApp')
+  .controller('FaqsCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
